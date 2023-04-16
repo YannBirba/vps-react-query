1f40a76bcd38752ca29bf0780c9d77b55843df43
YannBirba <birbayann@gmail.com>
2023-04-16 22:50:20 +0200

    fix: update app Link component to support base url

--- renderer/Link.tsx
+++ renderer/Link.tsx
@@ -3,17 +3,37 @@ import { usePageContext } from "./usePageContext";
 
 export { Link };
 
-function Link(props: {
+function Link({
+  href,
+  className,
+  children,
+}: {
   href?: string;
   className?: string;
   children: React.ReactNode;
 }) {
+  let base = import.meta.env.BASE_URL;
+  let processedHref = href;
+  if (base) {
+    if (!href?.startsWith("/")) {
+      throw new Error("Link href should start with /");
+    }
+    if (base.endsWith("/")) {
+      base = base.slice(0, -1);
+    }
+    processedHref = `${base}${href}`;
+  }
+
   const pageContext = usePageContext();
-  const className = [
-    props.className,
-    pageContext.urlPathname === props.href && "is-active",
+  const completeClassName = [
+    className,
+    pageContext.urlPathname === href && "is-active",
   ]
     .filter(Boolean)
     .join(" ");
-  return <a {...props} className={className} />;
+  return (
+    <a href={processedHref} className={completeClassName}>
+      {children}
+    </a>
+  );
 }
